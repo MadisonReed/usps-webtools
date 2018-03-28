29bd5c310bf90b07bf9812dd27c6787ba10d21d6
scottspork <scottspork@gmail.com>
2018-03-28 10:05:25 -0700

    Added revision tag to verify

--- src/usps.js
+++ src/usps.js
@@ -10,7 +10,7 @@ var usps = module.exports = function(config) {
   if (!(config && config.server && config.userId)) {
     throw 'Error: must pass usps server url and userId';
   }
-  if(!config.ttl){
+  if (!config.ttl) {
     config.ttl = 100000;
   }
 
@@ -31,6 +31,7 @@ var usps = module.exports = function(config) {
 */
 usps.prototype.verify = function(address, callback) {
   var obj = {
+    Revision: 1,
     Address: {
       FirmName: address.firm_name,
       Address1: address.street2 || '',
@@ -42,7 +43,7 @@ usps.prototype.verify = function(address, callback) {
     }
   };
 
-  if(address.urbanization) {
+  if (address.urbanization) {
     obj.Address.Urbanization = address.urbanization;
   }
 
@@ -61,13 +62,29 @@ usps.prototype.verify = function(address, callback) {
       zip4: address.Zip4
     };
 
-    if(address.FirmName) {
-      result.firm_name = address.FirmName;
-    }
+    var optional = {
+      FirmName: 'firm_name',
+      Address2Abbreviation: 'address2_abbreviation',
+      CityAbbreviation: 'city_abbreviation',
+      Urbanization: 'urbanization',
+      DeliveryPoint: 'delivery_point',
+      CarrierRoute: 'carrier_route',
+      Footnotes: 'footnotes',
+      DPVConfirmation: 'dpv_confirmation',
+      DPVCMRA: 'dpvcmra',
+      DPVFalse: 'dpv_false',
+      DPVFootnotes: 'dpv_footnotes',
+      Business: 'business',
+      CentralDeliveryPoint: 'central_delivery_point',
+      Vacant: 'vacant',
+    };
 
-    if(address.Urbanization) {
-      result.urbanization = address.Urbanization;
-    }
+    Object.keys(optional).forEach(function(key) {
+      var resultKey = optional[key];
+      if (address[key]) {
+        result[resultKey] = address[key];
+      }
+    });
 
     callback(null, result);
   });
@@ -123,35 +140,35 @@ usps.prototype.zipCodeLookup = function(address, callback) {
   @param {Function} callback The callback function
   @returns {Object} instance of module
 */
-usps.prototype.pricingRateV4 = function (pricingRate, callback) {
-    "use strict";
-    var obj = {
-        Package: {
-            '@ID'         : '1ST',
-            Service       : pricingRate.Service || 'PRIORITY',
-            ZipOrigination: pricingRate.ZipOrigination || 55401,
-            ZipDestination: pricingRate.ZipDestination,
-            Pounds        : pricingRate.Pounds,
-            Ounces        : pricingRate.Ounces,
-            Container     : pricingRate.Container,
-            Size          : pricingRate.Size,
-            Width         : pricingRate.Width,
-            Length        : pricingRate.Length,
-            Height        : pricingRate.Height,
-            Girth         : pricingRate.Girth,
-            Machinable   : pricingRate.Machinable,
-        }
-    };
+usps.prototype.pricingRateV4 = function(pricingRate, callback) {
+  "use strict";
+  var obj = {
+    Package: {
+      '@ID': '1ST',
+      Service: pricingRate.Service || 'PRIORITY',
+      ZipOrigination: pricingRate.ZipOrigination || 55401,
+      ZipDestination: pricingRate.ZipDestination,
+      Pounds: pricingRate.Pounds,
+      Ounces: pricingRate.Ounces,
+      Container: pricingRate.Container,
+      Size: pricingRate.Size,
+      Width: pricingRate.Width,
+      Length: pricingRate.Length,
+      Height: pricingRate.Height,
+      Girth: pricingRate.Girth,
+      Machinable: pricingRate.Machinable,
+    }
+  };
 
-    callUSPS('RateV4', 'RateV4', 'Package', this.config, obj, function (err, result) {
-        if (err) {
-            callback(err);
-            return;
-        }
-        
-        callback(null, result["Postage"]);
-    });
-    return this;
+  callUSPS('RateV4', 'RateV4', 'Package', this.config, obj, function(err, result) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    callback(null, result.Postage);
+  });
+  return this;
 };
 
 /**
@@ -213,7 +230,7 @@ function callUSPS(api, method, property, config, params, callback) {
       return;
     }
 
-    xml2js.parseString(body, {explicitArray: false}, function(err, result) {
+    xml2js.parseString(body, { explicitArray: false }, function(err, result) {
       var errMessage;
 
       if (err) {
@@ -228,7 +245,7 @@ function callUSPS(api, method, property, config, params, callback) {
       if (result.Error) {
         try {
           errMessage = result.Error.Description.trim();
-        } catch(e) {
+        } catch (e) {
           errMessage = result.Error;
         }
 
@@ -251,7 +268,7 @@ function callUSPS(api, method, property, config, params, callback) {
       if (specificResult.Error) {
         try {
           errMessage = specificResult.Error.Description.trim();
-        } catch(e) {
+        } catch (e) {
           errMessage = specificResult.Error;
         }
 
